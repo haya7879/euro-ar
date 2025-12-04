5d13e23f285bd2f0d95f52d7def28ad4a38deb86
sgok-mn <hayaalhosain.com@gmail.com>
2025-12-04 22:23:47 +0300

    Update reCAPTCHA site and secret keys in RECAPTCHA_CONFIG

--- constants/recaptcha.ts
+++ constants/recaptcha.ts
@@ -1,8 +1,8 @@
 // Get reCAPTCHA keys from: https://www.google.com/recaptcha/admin
 
 export const RECAPTCHA_CONFIG = {
-  siteKey: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || '',
-  secretKey: process.env.RECAPTCHA_SECRET_KEY || '',
+  siteKey: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || '6LezvtMrAAAAAD6czyI91m_cAMcGYZtQp4BN8hGW',
+  secretKey: process.env.RECAPTCHA_SECRET_KEY || '6LezvtMrAAAAACvOuR_8TES0QtmXrSb0zxh5JYqe',
   // Default actions for different forms
   actions: {
     contact: 'contact_form',
